2a9c10f6318ab84d1460f058f858a10057250025
Zaihang <1085212484@qq.com>
2019-07-15 11:18:30 +0800

    完成时间选择功能

--- lib/main.dart
+++ lib/main.dart
@@ -12,42 +12,68 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      home: MyHomePage(
-        life: Life(DateTime.now()),
-      ),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, @required this.life}) : super(key: key);
-
-  final Life life;
-
+  MyHomePage({Key key}) : super(key: key);
+  
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Life _life = Life(DateTime(2000, 01, 01));
+
+  void showDefaultYearPicker(BuildContext context) async {
+    final DateTime dateTime = await showDatePicker(
+      context: context,
+      initialDate: _life.birthDay,
+      firstDate: DateTime(1950, 1),
+      lastDate: DateTime.now(),
+      builder: (BuildContext context, Widget child) {
+        return Theme(
+          data: ThemeData.dark(),
+          child: child,
+        );
+      },
+    );
+    if (dateTime != null && dateTime != this._life.birthDay) {
+      setState(() {
+        this._life.birthDay=dateTime;
+
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    widget.life.birthDay = DateTime(2000, 01, 01);
     return Scaffold(
       appBar: AppBar(
-          title: Text(
-            DateTime.now().toString().substring(0, 10),
-            style: TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                fontFamily: "Barriecito"),
-          ),
-          centerTitle: true,
-          elevation: 0),
+        title: Text(
+          DateTime.now().toString().substring(0, 10),
+          style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              fontFamily: "Barriecito"),
+        ),
+        centerTitle: true,
+        elevation: 0,
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(Icons.settings),
+              onPressed: () {
+                showDefaultYearPicker(context);
+              })
+        ],
+      ),
       body: Padding(
         padding: const EdgeInsets.all(1.0),
         child: Column(
           children: <Widget>[
-            Grid(life: widget.life),
+            Grid(life: this._life),
             Padding(
               padding: const EdgeInsets.only(top: 40, bottom: 20),
               child: Row(
@@ -59,9 +85,9 @@ class _MyHomePageState extends State<MyHomePage> {
                         height: 48, child: Image.asset("asset/iron_man.png")),
                   ),
                   Text(
-                      widget.life.pastLife().toString() +
+                      this._life.pastLife().toString() +
                           " / " +
-                          widget.life.life.toString(),
+                          this._life.life.toString(),
                       style: TextStyle(fontSize: 48, fontFamily: "Barriecito")),
                 ],
               ),
