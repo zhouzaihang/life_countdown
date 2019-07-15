b6fcfcea92bf82d54be559915da3f2086e79225e
Zaihang <1085212484@qq.com>
2019-07-15 14:45:15 +0800

    完成生日储存功能

--- lib/life.dart
+++ lib/life.dart
@@ -1,21 +1,48 @@
-class Life {
+import 'package:scoped_model/scoped_model.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Life extends Model{
   int life = 900;
-  DateTime birthDay;
+  String _key;
+  DateTime _birthDay = DateTime(2000, 1, 1);
+
+  DateTime get birthDay => _birthDay;
+
+  set birthDay(DateTime value) {
+    _birthDay = value;
+    _setBirthday();
+    notifyListeners();
+  }
 
   static int dateDifference(DateTime date1, DateTime date2) {
     int result = date1.day - date2.day > 0 ? 1 : 0;
     return (date1.year - date2.year) * 12 + date1.month - date2.month + result;
   }
 
-  Life(DateTime birthDay) {
-    this.birthDay = birthDay;
+  Life(String key) {
+    this._key = key;
+    _initBirthday();
   }
 
   int pastLife() {
-    return dateDifference(DateTime.now(), this.birthDay);
+    return dateDifference(DateTime.now(), this._birthDay);
   }
 
   int remainingLife() {
     return life - pastLife();
+  }
+
+  Future _initBirthday() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    String birth = prefs.getString(_key) ?? null;
+    if (birth != null) {
+      this._birthDay = DateTime.parse(birth);
+    }
+    notifyListeners();
+  }
+
+  Future<bool> _setBirthday() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.setString(_key, _birthDay.toString());
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:life_countdown/grid.dart';
 import 'package:life_countdown/life.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(ScopedModel(
+    child: MyApp(),
+    model: Life("me"),
+  ));
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -12,25 +18,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primaryColor: Colors.white,
       ),
-      home: MyHomePage(),
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
-  
+class HomePage extends StatefulWidget {
+  HomePage({Key key}) : super(key: key);
+
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  Life _life = Life(DateTime(2000, 01, 01));
+class _HomePageState extends State<HomePage> {
+  Life _life;
+
+  @override
+  void initState() {
+    super.initState();
+    _life = ScopedModel.of<Life>(context);
+  }
 
   void showDefaultYearPicker(BuildContext context) async {
     final DateTime dateTime = await showDatePicker(
       context: context,
-      initialDate: _life.birthDay,
+      initialDate: this._life.birthDay,
       firstDate: DateTime(1950, 1),
       lastDate: DateTime.now(),
       builder: (BuildContext context, Widget child) {
@@ -41,10 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
       },
     );
     if (dateTime != null && dateTime != this._life.birthDay) {
-      setState(() {
-        this._life.birthDay=dateTime;
-
-      });
+      this._life.birthDay = dateTime;
     }
   }
 
@@ -71,35 +80,41 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(1.0),
-        child: Column(
-          children: <Widget>[
-            Grid(life: this._life),
-            Padding(
-              padding: const EdgeInsets.only(top: 40, bottom: 20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Padding(
-                    padding: EdgeInsets.only(right: 40),
-                    child: Container(
-                        height: 48, child: Image.asset("asset/iron_man.png")),
+        child: ScopedModelDescendant<Life>(
+          builder: (BuildContext context, Widget child, Life model) {
+            return Column(
+              children: <Widget>[
+                Grid(life: this._life),
+                Padding(
+                  padding: const EdgeInsets.only(top: 40, bottom: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(right: 40),
+                        child: Container(
+                            height: 48,
+                            child: Image.asset("asset/iron_man.png")),
+                      ),
+                      Text(
+                          this._life.pastLife().toString() +
+                              " / " +
+                              this._life.life.toString(),
+                          style: TextStyle(
+                              fontSize: 48, fontFamily: "Barriecito")),
+                    ],
                   ),
-                  Text(
-                      this._life.pastLife().toString() +
-                          " / " +
-                          this._life.life.toString(),
-                      style: TextStyle(fontSize: 48, fontFamily: "Barriecito")),
-                ],
-              ),
-            ),
-            Text(
-              "Stay Hungry \t Stay Foolish",
-              style: TextStyle(
-                  fontFamily: "Barriecito",
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24),
-            ),
-          ],
+                ),
+                Text(
+                  "Stay Hungry \t Stay Foolish",
+                  style: TextStyle(
+                      fontFamily: "Barriecito",
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24),
+                ),
+              ],
+            );
+          },
         ),
       ),
     );
